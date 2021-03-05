9a22dab22f1b68d5316afba99ba7d479af1aa574
apotato369550 <johnandresyap510@gmail.com>
2021-03-05 19:39:48 +0800

    Add Item Completion feature

--- src/components/todo.js
+++ src/components/todo.js
@@ -4,14 +4,25 @@ const Todo = ({ text, todo, todos, setTodos }) => {
     const handleDelete = () => {
         setTodos(todos.filter(
             (element) => element.id !== todo.id
-        ))
+        ));
     }
-    
+
+    const handleComplete = () => {
+        setTodos(todos.map((element) => {
+            if(element.id === todo.id){
+                console.log(element.id + " " + element.completed);
+                return {
+                    ...element, completed: !element.completed
+                }
+            }
+            return element;
+        }));
+    }
 
     return (
         <div className="todo">
-            <li className="todo-item">{ text }</li>
-            <button className="complete-btn">
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{ text }</li>
+            <button onClick={handleComplete} className="complete-btn">
                 <i className="fas fa-check"></i>
             </button>
             <button onClick={handleDelete} className="trash-btn">
